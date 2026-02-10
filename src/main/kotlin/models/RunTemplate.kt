6a620fb77a02b600af09c29fe2b6a07a643b86ee
tutosrive <tutosrivegamer@gmail.com>
2026-02-09 21:02:56 -0500

    First commit to my kotlin learn

---
+++ src/main/kotlin/models/RunTemplate.kt
@@ -0,0 +1,11 @@
+package com.srm.learn.models
+
+open class RunTemplate {
+    open fun run(){
+        println("\"RunTemplate.run()\" method ...")
+    }
+
+    open fun doAll(){
+        println("\"RunTemplate.todoAll()\" method...")
+    }
+}
---
+++ src/main/kotlin/topics/Types1.kt
@@ -0,0 +1,30 @@
+package com.srm.learn.topics
+
+import com.srm.learn.models.RunTemplate
+
+class Types1: RunTemplate() {
+
+    override fun run(){
+        this.doAll()
+    }
+
+    override fun doAll() {
+        val variables: Array<Any> = this.variablesSet()
+
+        for(varI in variables){
+            println("Variable Value: [$varI]")
+        }
+    }
+
+    fun variablesSet(): Array<Any> {
+        val int: Int = 12
+        val float: Float = 12.1f
+        val double: Double = 12.2
+        val char: Char = 'a'
+        val str: String = "This is a text"
+
+        val arrAny: Array<Any> = arrayOf(int, float, double, char, str)
+        return arrAny
+    }
+
+}
