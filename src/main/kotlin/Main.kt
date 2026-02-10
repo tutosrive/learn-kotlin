a8cabf26807762c9b8939628e3607bea1d3e23ab
Tutos Rive <108991712+tutosrive@users.noreply.github.com>
2026-02-09 21:46:01 -0500

    Remove idea comment from main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,8 +2,6 @@ package com.srm.learn
 import com.srm.learn.models.RunTemplate
 import com.srm.learn.topics.Types1
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
     val typesObj: Types1 = Types1()
     val arrAllObj: Array<RunTemplate> = arrayOf(typesObj)
