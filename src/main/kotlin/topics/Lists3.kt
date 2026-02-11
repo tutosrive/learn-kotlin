efbb49da0055ef97b0621e1284f9113ed4986963
tutosrive <tutosrivegamer@gmail.com>
2026-02-11 15:11:23 -0500

    Fix commit to lists topic

--- src/main/kotlin/topics/Lists3.kt
+++ src/main/kotlin/topics/Lists3.kt
@@ -1,4 +1,25 @@
 package com.srm.learn.topics
 
-class Lists3 {
+import com.srm.learn.models.RunTemplate
+
+class Lists3 : RunTemplate(t = "Listas (List)") {
+    override fun doAll() {
+        val lists: List<Any> = this.createLists()
+
+        for (list in lists) {
+            println("Objeto Lista: \n=> $list")
+            //println("Primer elemento: ${list.first()}")
+        }
+    }
+
+    fun createLists(): List<Any> {
+        // Lista de solo lectura
+        val pairNumbers: List<Int> = listOf(2, 4, 6, 8, 10, 12, 14, 16, 18)
+
+        // Lista mutable
+        val users = mutableListOf<String>("Santiago", "Nicrolax", "Eduardo")
+
+        val allLists = listOf<Any>(users)
+        return allLists
+    }
 }
